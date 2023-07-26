ca76781d3cc994a57dfd892b179f48c321b94566
Win Barua <win.barua2@gmail.com>
2023-07-26 19:08:14 +0100

    Setup Nuxt project with Tailwind and Typescript

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <p>about</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <h1 class="">Hello world</h1>
+    <p>Home</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'bg-gray-600'
+  },
+});
+
+</script>
+
+<style scoped></style>
