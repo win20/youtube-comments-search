132ad09a339b2b3195369647201998ecb805dfda
Win Barua <win.barua2@gmail.com>
2023-07-28 18:00:45 +0100

    Toggling mobile sidebar

--- components/MobileNavigation.vue
+++ components/MobileNavigation.vue
@@ -6,13 +6,17 @@
 			<span :class="enabled ? 'translate-x-6' : 'translate-x-1'"
 				class="inline-block h-5 w-5 transform rounded-full bg-white transition bg-gradient-to-tr from-gradientOrange to-gradientRed" />
 		</Switch>
-		<Icon name="iconamoon:menu-burger-horizontal-bold" color="white" size="28" class="" />
+		<button @click="toggleSidebar">
+			<Icon name="iconamoon:menu-burger-horizontal-bold" color="white" size="28" class="" />
+		</button>
 	</div>
-	<div class="bg-white absolute right-0 top-0 w-6/12 h-full">
-		<Icon name="basil:cross-outline" color="black" size="40" class="absolute right-0" />
+	<div class="bg-gray-300 absolute right-0 top-0 w-6/12 h-full" v-if="isSidebarActive">
+		<buton @click="toggleSidebar">
+			<Icon name="basil:cross-outline" color="black" size="40" class="absolute right-0" />
+		</buton>
 		<ul class="text-center relative space-y-16 mt-[50%]">
-			<li>Report an issue</li>
-			<li>Github</li>
+			<li><a href="#">Report an issue</a></li>
+			<li><a href="#">Github</a></li>
 		</ul>
 
 		<span class="absolute bottom-2 right-3 ml-5 text-sm font-thin">10 searches made</span>
@@ -24,6 +28,11 @@ import { ref } from 'vue';
 import { Switch } from '@headlessui/vue';
 
 const enabled = ref(false);
+const isSidebarActive = ref(false);
+
+const toggleSidebar = () => {
+	isSidebarActive.value = !isSidebarActive.value;
+};
 </script>
 
 <style scoped></style>
