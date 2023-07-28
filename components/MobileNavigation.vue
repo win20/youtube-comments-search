467baf8d5a6abe92df8b05bf0aadec55e1045472
Win Barua <win.barua2@gmail.com>
2023-07-28 18:11:12 +0100

    Transition for sidebar

--- components/MobileNavigation.vue
+++ components/MobileNavigation.vue
@@ -10,17 +10,19 @@
 			<Icon name="iconamoon:menu-burger-horizontal-bold" color="white" size="28" class="" />
 		</button>
 	</div>
-	<div class="bg-gray-300 absolute right-0 top-0 w-6/12 h-full" v-if="isSidebarActive">
-		<buton @click="toggleSidebar">
-			<Icon name="basil:cross-outline" color="black" size="40" class="absolute right-0" />
-		</buton>
-		<ul class="text-center relative space-y-16 mt-[50%]">
-			<li><a href="#">Report an issue</a></li>
-			<li><a href="#">Github</a></li>
-		</ul>
+	<Transition name="slide-fade">
+		<div class="bg-gray-300 absolute right-0 top-0 w-6/12 h-full" v-if="isSidebarActive">
+			<buton @click="toggleSidebar">
+				<Icon name="basil:cross-outline" color="black" size="40" class="absolute right-0" />
+			</buton>
+			<ul class="text-center relative space-y-16 mt-[50%]">
+				<li><a href="#">Report an issue</a></li>
+				<li><a href="#">Github</a></li>
+			</ul>
 
-		<span class="absolute bottom-2 right-3 ml-5 text-sm font-thin">10 searches made</span>
-	</div>
+			<span class="absolute bottom-2 right-3 ml-5 text-sm font-thin">10 searches made</span>
+		</div>
+	</Transition>
 </template>
 
 <script setup lang="ts">
@@ -35,4 +37,18 @@ const toggleSidebar = () => {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.slide-fade-enter-active {
+	transition: all 0.2s ease-in-out;
+}
+
+.slide-fade-leave-active {
+	transition: all 0.2s ease-in-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+	transform: translateX(20px);
+	opacity: 0;
+}
+</style>
