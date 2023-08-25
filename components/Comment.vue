d7eb4cd3dbb38b21ac628bf1fa11f17ad8a3535e
Win Barua <win.barua2@gmail.com>
2023-08-25 23:07:00 +0100

    Image crossorigin

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-6 w-[85%] mx-auto mt-4">
-    <img :src="props.avatar" alt="avatar image" class="w-12 h-12 rounded-full">
+    <img crossorigin="anonymous" :src="props.avatar" alt="avatar image" class="w-12 h-12 rounded-full">
     <span class="text-left col-span-5 ml-2">
       <div class="mb-2">{{ props.user }}</div>
       <div class="text-sm">{{ props.commentText }}</div>
